8b9dc613553889b7a98c2c7273a739f2a61b5bf9
Larry Dawson <larry.dawson@gmail.com>
2025-07-25 20:27:12 -0500

    Big cleanup

--- verify_package.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, './utils/mcp_manager/src')
-
-try:
-    from mcp_manager.commands import install
-    print("Successfully imported mcp_manager.commands.install")
-except ImportError as e:
-    print(f"Failed to import mcp_manager.commands.install: {e}")
